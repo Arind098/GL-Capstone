8c4afdda4df115fb8748d9e43f78e6d878a41680
Arind098 <mishra.arind@gmail.com>
2019-03-05 05:54:47 +0000

    Add files via upload

---
+++ install_missing.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update
+python -m pip install --user numpy \
+                    matplotlib \
+                    requests \
+                    re \
+                    seaborn \
+                    pydub \
+                    features \
+                    scipy
